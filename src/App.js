bbf81527f434e87da208c0f71e1a4f40486de169
Juan A. Reyes <jareyesda@gmail.com>
2022-06-24 21:22:35 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="content-width-extra-large home-container">
+      <div>
+        <h1 class="display-heading-2 display-inline-block">
+        Hi, I'm Juan 👋🏻
+        </h1>
+      </div>
+      <div>
+        <h3>
+          a software developer @ <a href="https://www.inspiringapps.com/" target="_blank">InspiringApps</a>
+        </h3>
+      </div>
+      <div class="section wf-section">
+        <div class="container grid-container">
+          <div class="w-layout-grid-fullscreen-section-content">
+            <div class="panel bg-gradient-7">
+              <div class="panel-body panel-body-large align-content-center rounded purposepadding">
+                <div class="center-content">
+                  <div class="content-width-large mobiletext">
+                    <h1 class="heading-3">
+                      About me
+                    </h1>
+                    <div class="text-block-2">
+                      <p>I am a Florida, U.S based software engineer especializing in iOS and Swift development - with a hint of web. I graduated from the University of South Florida with a Bachelor's of Science in Mechanical Engineering - and advocate for open source Swift projects and using Swift outside of iOS development.</p>
+                      <p>When I am not developing award-winning iOS applications for InspiringApps, you can catch me rock climbing 🧗🏻‍♂️, dabbling in other tech stacks 👨🏻‍💻, or finding ways to contribute to Swift open source projects 🧠</p>
+                      <p>Want to connect? Don't be a stranger! <a href='mailto:hello@juan.codes'>hello@juan.codes</a> </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
